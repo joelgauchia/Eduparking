63b405d9db9e2c454f0c09965c283626a5c88599
Joel Gauchia <joelgauchia@gmail.com>
2023-03-09 15:52:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/eduparking/ui/search/SearchFragment.java
@@ -0,0 +1,39 @@
+package com.example.eduparking.ui.search;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.eduparking.databinding.FragmentSearchBinding;
+
+public class SearchFragment extends Fragment {
+
+    private FragmentSearchBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SearchViewModel searchViewModel =
+                new ViewModelProvider(this).get(SearchViewModel.class);
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSearch;
+        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
